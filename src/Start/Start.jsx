7fb426f78d7280476a72af4c4f03fb236d845bf4
2003-dilip <150585626+2003-dilip@users.noreply.github.com>
2024-05-13 19:38:52 +0530

    Update Start.jsx

--- src/Start/Start.jsx
+++ src/Start/Start.jsx
@@ -50,7 +50,7 @@ const Start = () => {
         <div className="splash-text">
           <h1>Welcome To Green Gage💚 </h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat iusto nobis officia inventore excepturi, cum repellat, odit sit magni fuga cumque nesciunt sed aperiam? Tempora magni possimus dolores assumenda iusto laboriosam sequi dignissimos, aut repellendus.
+            Dive into a world where words and images converge to create powerful narratives. At our platform, we celebrate the art of storytelling and visual expression. Whether you're a seasoned writer or a budding artist, our community welcomes you to share your voice and showcase your creativity. Join us today to embark on a journey of exploration, connection, and inspiration. Together, let's bring stories to life and make an impact through the beauty of art and storytelling.
           </p>
         </div>
   
